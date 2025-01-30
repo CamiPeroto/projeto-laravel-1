77e2ed822f51d9ce061b719160206d5ffeab8934
CamiPeroto <camilabuenoperoto@gmail.com>
2025-01-30 15:45:04 -0300

    Implementando o create e o read

---
+++ app/Models/Course.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    //indicar o nome da tabela
+    protected $table = 'courses';
+
+    //indicar quais colunas podem ser preenchidas
+    protected $fillable = ['name'];
+}
--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -4,7 +4,23 @@
     <h2>Listar os cursos</h2>
 
     <a href="{{ route('courses.show') }}">Visualizar</a><br>
-    <a href="{{ route('courses.create') }}">Cadastrar</a>    
+    <a href="{{ route('courses.create') }}">Cadastrar</a><br>   
+
+    {{-- imprimir os registros --}}
+    @forelse ($courses as $course)
+    {{$course->id}}<br>
+    {{$course->name}}<br>
+    {{\Carbon\Carbon::parse($course->created_at)->format('d/m/Y H:i:s')}}<br>
+    {{\Carbon\Carbon::parse($course->updated_at)->format('d/m/Y H:i:s')}}<br>
+    <hr>
+        
+    @empty
+        <p style="color: #f00"> Nenhum curso encontrado!</p>
+    @endforelse
+    {{-- imprimir a paginação --}}
+    {{-- {{$courses->links()}} --}}
+    
+    {{-- {{dd($courses)}} --}}
 @endsection
     
  
