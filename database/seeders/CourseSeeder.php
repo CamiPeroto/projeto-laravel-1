a5e78aa2102c78c2bf23eef0d919f46a0bd65959
CamiPeroto <camilabuenoperoto@gmail.com>
2025-02-05 14:26:31 -0300

    Criando a table price e atualizando na seeder

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -16,16 +16,19 @@ class CourseSeeder extends Seeder
         if(!Course::where('name','Curso de Laravel - T1' )->first()){
         Course::create([
             'name' => 'Curso de Laravel - T1',
+            'price' => 197.43,
         ]);
     }
          if(!Course::where('name','Curso de Laravel - T2' )->first()){
         Course::create([
             'name' => 'Curso de Laravel - T2',
+            'price' => 197.47,
         ]);
     }
     if(!Course::where('name','Curso de Laravel - T3' )->first()){
         Course::create([
             'name' => 'Curso de Laravel - T3',
+            'price' => 277.45,
            
         ]);
     }
