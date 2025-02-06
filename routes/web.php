84683c049379eda5bbc0b932be8170ccbc09395b
CamiPeroto <camilabuenoperoto@gmail.com>
2025-02-06 16:57:31 -0300

    Criando as rotas de show, create e store das aulas

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::delete('/destroy-course/{course}', [CourseController::class, 'destroy'])-
 
 //Aulas
 Route::get('/index-classe/{course}', [ClasseController::class, 'index'])->name('classe.index');
-
+Route::get('/show-classe/{classe}', [ClasseController::class, 'show'])->name('classe.show');
+Route::get('/create-classe/{course}', [ClasseController::class, 'create'])->name('classe.create');
+Route::get('/store-classe', [ClasseController::class, 'store'])->name('classe.store');
