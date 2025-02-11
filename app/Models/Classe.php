6775ba07094de13e1d17f861e7800b6533108777
CamiPeroto <camilabuenoperoto@gmail.com>
2025-02-11 14:56:01 -0300

    Implementando o laravel audit

--- app/Models/Classe.php
+++ app/Models/Classe.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
+use \OwenIt\Auditing\Auditable as AuditingAuditable;
+use OwenIt\Auditing\Contracts\Auditable ;
 
-class Classe extends Model
+
+class Classe extends Model implements Auditable
 {
+       use HasFactory, AuditingAuditable;
        //indicar o nome da tabela
        protected $table = 'classes';
 
