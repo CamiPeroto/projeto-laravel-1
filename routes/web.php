86745d46d46d495e630f09b8e0293abae149d93e
CamiPeroto <camilabuenoperoto@gmail.com>
2025-02-20 19:23:38 -0300

    Implementando o middleware nas rotas

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,13 @@ Route::post('/login', [LoginController::class, 'loginProcess'])->name('login.pro
 //Sair do sistema
 Route::get('/logout', [LoginController::class, 'destroy'])->name('login.destroy'); 
 
+
+//Rotas privadas
+Route::group(['middleware' => 'auth'], function()
+{
 // Dashboard
 Route::get('/index-dashboard', [DashboardController::class, 'index'])->name('dashboard.index'); 
-
 // Rota, método pra criar na controller e nome de referência p/ view
-
 // //Usuários
 Route::get('/index-user', [UserController::class, 'index'])->name('user.index');
 Route::get('/show-user/{user}', [UserController::class, 'show'])->name('user.show');
@@ -30,11 +32,9 @@ Route::get('/create-user', [UserController::class, 'create'])->name('user.create
 Route::post('/store-user', [UserController::class, 'store'])->name('user.store');
 Route::get('/edit-user/{user}', [UserController::class, 'edit'])->name('user.edit');
 Route::put('/update-user/{user}', [UserController::class, 'update'])->name('user.update');
-
 Route::get('/edit-user-password/{user}', [UserController::class, 'editPassword'])->name('user.edit-password');
 Route::put('/update-user-password/{user}', [UserController::class, 'updatePassword'])->name('user.update-password');
 Route::delete('/destroy-user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
-
 //Cursos
 Route::get('/index-course', [CourseController::class, 'index'])->name('course.index'); //listar os cursos
 Route::get('/show-course/{course}', [CourseController::class, 'show'])->name('course.show'); //control + D para selecionar a mesma palavra e editar
@@ -43,7 +43,6 @@ Route::post('/store-course', [CourseController::class, 'store'])->name('course.s
 Route::get('/edit-course/{course}', [CourseController::class, 'edit'])->name('course.edit');
 Route::put('/update-course/{course}', [CourseController::class, 'update'])->name('course.update'); //put recomendado para atualizar no banco
 Route::delete('/destroy-course/{course}', [CourseController::class, 'destroy'])->name('course.destroy'); // delete para apagar registros
-
 //Aulas
 Route::get('/index-classe/{course}', [ClasseController::class, 'index'])->name('classe.index');
 Route::get('/show-classe/{classe}', [ClasseController::class, 'show'])->name('classe.show');
@@ -52,3 +51,5 @@ Route::post('/edit-classe', [ClasseController::class, 'store'])->name('classe.st
 Route::get('/edit-classe/{classe}', [ClasseController::class, 'edit'])->name('classe.edit');
 Route::put('/update-classe/{classe}', [ClasseController::class, 'update'])->name('classe.update'); //put recomendado para atualizar no banco
 Route::delete('/destroy-classe/{classe}', [ClasseController::class, 'destroy'])->name('classe.destroy'); // delete para apagar registros
+
+}); //fim do middleware
